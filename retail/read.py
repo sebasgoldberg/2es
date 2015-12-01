1270cbe89898221852e938a44753321f4100179f
Juan Sebastian Goldberg <sebas.goldberg@gmail.com>
2015-12-01 15:21:53 -0200

    Se inicia la implementación del merge de los indices a ser incluidos en el indice retail.

--- retail/read.py
+++ retail/read.py
@@ -13,31 +13,64 @@ L=Lang.get_instance()
 
 INDEX = 'retail'
 TYPE = 'retail'
-FILE_NAME_PREFIX = '../retail'
+FILE_NAME_PREFIX = 'retail/retail'
 
-def read(data):
+es = Elasticsearch(['10.151.1.21',])
+efg = ElasticFilesGenerator(INDEX, TYPE, FILE_NAME_PREFIX)
 
-    es = Elasticsearch(['10.151.1.21',])
-    efg = ElasticFilesGenerator(INDEX, TYPE, FILE_NAME_PREFIX)
+def retail_id(x):
+    return "%s%s" % (x['_source'][L.matid], str(x['_source'][L.data][0:10]).replace('-',''))
 
-    data_desde = datetime.datetime.combine(data,datetime.datetime.min.time())
-    data_ate = datetime.datetime.combine(data,datetime.datetime.max.time())
+def read_index(index, data_desde, data_ate):
+    
+    res = scan(es, {
+        "query" : {
+            "filtered" : {
+                "filter" : {
+                    "range" : {
+                        "data" : {
+                            "gte" : data_desde.strftime("%Y-%m-%d %H:%M:%S"),
+                            "lte" : data_ate.strftime("%Y-%m-%d %H:%M:%S")
+                            } } } } } }, index=index)
 
-    #res = es.search(index="venda", body={"query": {"match_all": {}}}, search_type='scan')
+    res = sorted(res, key=retail_id)
 
-    res = scan(es, {"query": {"match_all": {}}}, index="venda")
 
-    for i in res:
-        print i['_id']
-        print i['_source']
+def read(data_desde=datetime.date.today(), data_ate=None):
 
-    """
-    print("Got %d Hits:" % res['hits']['total'])
+    if data_ate is None:
+        data_ate = data_desde
 
-    for hit in res['hits']['hits']:
-        register = hit['_source']
-        #efg.add(register, "%s%s" % (register[L.matid], str(register[L.data][0:10]).replace('-','')))
-        print register
-    """
+    data_desde = datetime.datetime.combine(data_desde,datetime.datetime.min.time())
+    data_ate = datetime.datetime.combine(data_ate,datetime.datetime.max.time())
 
-read(datetime.date(2015,11,25))
+    venda = read_index('venda', data_desde, data_ate)
+    quebra = read_index('quebra', data_desde, data_ate)
+    ruptura = read_index('ruptura', data_desde, data_ate)
+
+    todos_los_registros_leidos = True
+    ivenda, iquebra, iruptura = 0, 0, 0
+
+    while not todos_los_registros_leidos:
+        idvenda = retail_id(venda[ivenda])
+        idquebra = retail_id(quebra[iquebra])
+        idruptura = retail_id(ruptura[iruptura])
+
+        if idvenda == idquebra:
+            if idquebra == idruptura:
+                register = venda[ivenda]['_source']
+                register.update(quebra[iquebra]['_source'])
+                register.update(ruptura[iruptura['_source'])
+                efg.add(register, "%s%s" % (register[L.matid], str(register[L.data][0:10]).replace('-','')))
+                ivenda = ivenda + 1
+                iquebra = iquebra + 1
+                iquebra = iquebra + 1
+            elif idquebra < idruptura:
+
+if len(sys.argv) == 1:
+    read()
+elif len(sys.argv) == 2:
+    read(datetime.datetime.strptime(sys.argv[1],"%d.%m.%Y"))
+elif len(sys.argv) == 3:
+    read(datetime.datetime.strptime(sys.argv[1],"%d.%m.%Y"),
+        datetime.datetime.strptime(sys.argv[2],"%d.%m.%Y"))
