bd095e74d0e49004749152638b43e99b04a5a124
Juan Sebastian Goldberg <sebas.goldberg@gmail.com>
2015-12-17 09:21:08 -0200

    Ubuntu to Centos migration.

--- els/upload.sh
+++ els/upload.sh
@@ -2,5 +2,5 @@
 
 for i in "$@"
 do
-  /usr/bin/curl -XPUT 10.151.1.21:9200/_bulk --data-binary "@$i" > /dev/null
+  /usr/bin/curl -XPUT localhost:9200/_bulk --data-binary "@$i" > /dev/null
 done
--- ruptura/prepare-files.sh
+++ ruptura/prepare-files.sh
@@ -1,3 +1,8 @@
- #!/bin/bash
- /usr/bin/dos2unix $@
- /usr/bin/rename 's/(.*)\/.*_([0-9]*)_([0-9]*)_([0-9]*).*/$1\/$4-$3-$2-ruptura/g' $@
+#!/bin/bash
+
+dos2unix "$@"
+
+for i in "$@"; do
+    NEW_FILE_NAME="$(echo "$i" | sed 's/\(.*\)\/.*_\([0-9]*\)_\([0-9]*\)_\([0-9]*\).*/\1\/\4-\3-\2-ruptura/g')"
+    mv "$i" "$NEW_FILE_NAME"
+done
